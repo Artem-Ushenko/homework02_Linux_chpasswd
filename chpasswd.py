744a5f21ac09a1de02c4b4f7da59399d71336db4
Artem Ushenko <aushenko34@gmail.com>
2023-07-27 15:16:13 +0000

    Was fixed some errors

--- chpasswd.py
+++ chpasswd.py
@@ -5,7 +5,8 @@ import string
 import random
 import pexpect
 
-
+#Functions place
+#-----------------------------------------------------------------------------------------------------
 def generator_password(length_password):
     all_symbols = string.ascii_letters + string.digits + string.punctuation
 
@@ -28,14 +29,8 @@ def search_pass_user(user):
         else:
             return 1
 
-def change_password(password, user_name, what_user_check):
+def change_password(password, user_name):
     set_password = pexpect.spawn(f"passwd {user_name}")
-    
-    if what_user_check != 0:
-        
-        set_password.expect('(current) UNIX password:')
-        set_password.sendline(input())
-
     set_password.expect('Enter new UNIX password:')
     set_password.sendline(password)
     set_password.expect('Retype new UNIX password:')
@@ -43,7 +38,10 @@ def change_password(password, user_name, what_user_check):
     set_password.expect('passwd: password updated successfully')
     set_password.expect(pexpect.EOF)
 
-    return f"Your login: {user_name}/nand password: {password}"
+    return f"Your login: {user_name}\nand password: {password}"
+
+#--------------------------------------------------------------------------------------------------
+
 
 user_name = input("Enter your username: ")
 
@@ -59,25 +57,21 @@ else:
     if check_exist_password == 0:
         print("You do not have a password yet !!!")
         
-        what_user_check = 0
-        
         user_choice_with_no_password = input("If you want to generate and set password to your account, please enter 'yes': ")
         if user_choice_with_no_password == 'yes':
             length_password_for_user_with_no_password = int(input("Please enter length password: "))
             password = generator_password(length_password_for_user_with_no_password)
-            print(change_password(password, user_name, what_user_check))
+            print(change_password(password, user_name))
         else:
             exit()
     else:
         print("Good !!! Password already exists !")
 
-        what_user_check = 1
-
         user_choice_with_password = input("If you want to generate and set new password to your account, please enter 'yes': ")
         if user_choice_with_password == 'yes':
             length_password_for_user_with_password = int(input("Please enter length password: "))
             password = generator_password(length_password_for_user_with_password)
-            print(change_password(password, user_name, what_user_check))
+            print(change_password(password, user_name))
 
         else:
             exit()
